7bc94ab4430140460187d9e1335af222edcf7e43
ClaytonTaylor <claytaylor@cox.net>
2016-07-07 17:04:45 -0600

    Loop-Practice1 Complete

---
+++ JS-Loop-Practice.js
@@ -0,0 +1,72 @@
+// # 1
+
+/*
+
+for(var i = 0; i < 501; i+=100){
+    console.log(i);
+}
+
+*/
+
+// #2
+
+/*
+
+for(var i = 1; i < 65; i=i*2) {
+  	console.log(i);
+}
+
+*/
+
+// #3
+
+/*
+
+for(var i = 1; i < 4; i++)
+for(j = 1 ; j <= 3 ; j++) {
+    console.log(i);
+}
+
+*/
+
+// #4
+
+/*
+
+for(var i = 0; i < 12; i+=2){
+    console.log(i);
+}
+
+*/
+
+// #5
+
+/*
+
+for(var i = 3; i <= 15; i+=3){
+    console.log(i);
+}
+
+*/
+
+// #6
+
+/*
+
+for(var i = 9; i > -1; i--){
+    console.log(i);
+}
+
+*/
+
+// #7
+
+/*
+
+for(var i = 1; i < 4; i++){
+for(var j = 0; j <= 3; j++) {
+	console.log(j);
+}
+}
+
+*/
